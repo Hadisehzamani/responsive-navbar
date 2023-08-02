41c3470bc1ba0a3b53e4b8d771f9398b0255486a
Hadisehzamani <hadisehzmn@gmail.com>
2023-08-02 20:52:59 +0330

    edit navLinks

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let hamburger = document.querySelector('.hamburger')
+let navlinks = document.querySelectorAll('.links');
 
 hamburger.onclick = function(){
     let navbar = document.querySelector('.nav-bar')
     navbar.classList.toggle('active')
 }
+
+navlinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        navlinks.forEach(function(item){
+            item.style.backgroundColor = '#11101b'
+            item.style.color = '#fefefe'
+        })
+        e.target.style.backgroundColor = '#fefefe'
+        e.target.style.color = '#11101b'
+    });
+});
