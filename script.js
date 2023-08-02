2d260e1345ebcf24dfb3d770565d9fcb9cea32a0
Hadisehzamani <hadisehzmn@gmail.com>
2023-08-02 16:23:01 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let hamburger = document.querySelector('.hamburger')
+
+hamburger.onclick = function(){
+    let navbar = document.querySelector('.nav-bar')
+    navbar.classList.toggle('active')
+}
